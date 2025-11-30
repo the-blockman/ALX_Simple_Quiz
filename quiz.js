3b92b5a7fd05d2028220790404aeb922241126af
the-blockman <105639638+the-blockman@users.noreply.github.com>
2025-11-30 19:32:23 +0100

    Refactor checkAnswer function to remove commented code and improve readability

--- quiz.js
+++ quiz.js
@@ -4,16 +4,16 @@ function checkAnswer() {
   const correctAnswer = "4";
   let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
 
-  /*if (userAnswer == correctAnswer) {
+  if (userAnswer == correctAnswer) {
     feedback.textContent = "Correct! Well done.";
   } else {
     feedback.textContent = "That's incorrect. Try again!";
-  }*/
+  }
 
-  feedback.textContent =
+  /*feedback.textContent =
     userAnswer == correctAnswer
       ? "Correct! Well done."
-      : "That's incorrect. Try again!";
+      : "That's incorrect. Try again!";*/
 }
 
 const submit = document.getElementById("submit-answer");
