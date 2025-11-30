e5f35d4a91795c6a13d5b26ae83d1335ab057ac3
the-blockman <105639638+the-blockman@users.noreply.github.com>
2025-11-30 12:00:41 +0100

    Refactor checkAnswer function to simplify feedback logic

--- quiz.js
+++ quiz.js
@@ -1,4 +1,17 @@
+const feedback = document.getElementById("feedback");
+
 function checkAnswer() {
   const correctAnswer = "4";
   let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+
+  /*if (userAnswer == correctAnswer) {
+    feedback.textContent = "Correct! Well done.";
+  } else {
+    feedback.textContent = "That's incorrect. Try again!";
+  }*/
+
+  feedback.textContent =
+    userAnswer == correctAnswer
+      ? "Correct! Well done."
+      : "That's incorrect. Try again!";
 }
