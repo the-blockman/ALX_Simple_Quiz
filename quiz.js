f957a27633102b4dcd3d40345028b7045b9e5473
the-blockman <105639638+the-blockman@users.noreply.github.com>
2025-11-30 19:34:08 +0100

    Use strict equality check in checkAnswer function

--- quiz.js
+++ quiz.js
@@ -4,7 +4,7 @@ function checkAnswer() {
   const correctAnswer = "4";
   let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
 
-  if (userAnswer == correctAnswer) {
+  if (userAnswer === correctAnswer) {
     feedback.textContent = "Correct! Well done.";
   } else {
     feedback.textContent = "That's incorrect. Try again!";
